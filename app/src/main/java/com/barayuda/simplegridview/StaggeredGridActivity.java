a341ab7438117d9d2675b763d94c98f9a5a156e0
Barayuda Gautama <saya.bara@gmail.com>
2017-05-05 10:24:58 +0800

    Add new images

--- app/src/main/java/com/barayuda/simplegridview/StaggeredGridActivity.java
+++ app/src/main/java/com/barayuda/simplegridview/StaggeredGridActivity.java
@@ -14,16 +14,16 @@ public class StaggeredGridActivity extends AppCompatActivity {
 
     private StaggeredGridView staggeredGridView;
     private String[] items = new String[]{
-            "https://s-media-cache-ak0.pinimg.com/736x/31/72/90/3172904c6591b127ac71b66b300a87f4.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/c4/93/d1/c493d196b1b4e0ff61d8cb4a13727916.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/54/a3/23/54a323dcafda85b7fda6883c00f2860a.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/80/d5/b8/80d5b8ccfef474f5c895400acdbea63b.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/0e/d7/7a/0ed77a3208f982a6f552fbf30f7d4a4c.jpg",
+            "https://s-media-cache-ak0.pinimg.com/474x/59/bf/3f/59bf3fa0c94e185ddddf2a0d53a1f1dd.jpg",
+            "https://s-media-cache-ak0.pinimg.com/564x/17/a7/67/17a7674032d40de6cc218af3f14061d3.jpg",
+            "https://s-media-cache-ak0.pinimg.com/474x/39/aa/9d/39aa9db90d3e4b06c1eff382d93ebb57.jpg",
+            "https://s-media-cache-ak0.pinimg.com/474x/a3/ab/73/a3ab7374c2eb919cfffac23eace01033.jpg",
+            "https://s-media-cache-ak0.pinimg.com/564x/77/bc/72/77bc72c07f04f2cfad9f8b6aa8209159.jpg",
             "https://s-media-cache-ak0.pinimg.com/736x/7f/2e/79/7f2e790b6ebb34699b887181d7a6a3ed.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/fd/66/f0/fd66f045d95449469fa8b41b4b975edb.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/d7/57/ca/d757ca1df5224dc46633a42e3c34bb90.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/96/2a/d1/962ad10951a221f4ca26bb08e911da10.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/ac/89/c7/ac89c7aaef5754bfd5d7b4a698fdb6cc.jpg"
+            "https://s-media-cache-ak0.pinimg.com/564x/86/80/be/8680be8b94efb4ea78f8e58a0b1bc803.jpg",
+            "https://s-media-cache-ak0.pinimg.com/564x/37/43/37/37433769be71de494020524cfa643659.jpg",
+            "https://s-media-cache-ak0.pinimg.com/564x/e4/84/ee/e484ee54682924e1a70dfb90b8e3d952.jpg",
+            "https://s-media-cache-ak0.pinimg.com/564x/1e/09/41/1e0941bb6d1c09108347d04ffe8924cc.jpg"
     };
 
     @Override
@@ -31,7 +31,7 @@ public class StaggeredGridActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staggered_grid);
 
-        getSupportActionBar().setTitle("Star Wars");
+        getSupportActionBar().setTitle("Power Rangers");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         staggeredGridView = (StaggeredGridView)findViewById(R.id.gv_staggered);
